4715d8e34668a52b748f93a330d502f4eaab7c39
abishek.paudel <abishek.paudel@swifttech.com.np>
2026-06-18 11:28:43 +0545

    basic started for golang

---
+++ control_flow/control_flow.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var age int = 16
+
+	if age >=18{
+		fmt.Println("adult")
+	}else
+	{
+		fmt.Println("minior")
+	}
+
+	var a float64 = 3.555
+	var b int = 3
+	
+	if a==3.555 && b ==3 {
+		fmt.Println("condition satisfied")
+	}
+}
---
+++ fundamentals/variable.go
@@ -0,0 +1,34 @@
+
+package main
+
+import "fmt"
+
+
+func main(){
+
+
+	// defining string
+city := "kathmandu Nepal"
+fmt.Println(city)
+
+var name string = "Abishek paudel"
+fmt.Println(name)
+
+// float
+var floatValue float64 = 3.1444
+fmt.Println(floatValue)
+
+
+// integer
+var intergerValue int = 200
+fmt.Println(intergerValue)
+
+// bool
+
+var newBooleanValue bool = true;
+fmt.Println(newBooleanValue)
+
+
+
+}
+
